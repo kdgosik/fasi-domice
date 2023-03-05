bd5cb1a1f8ac599b9eb0053542274b9c9dfd916e
Kirk Gosik <kirkgosik@Kirks-MacBook-Air.local>
2023-03-05 12:31:55 -0500

    created setup script

---
+++ setup.R
@@ -0,0 +1,33 @@
+install.packages("googledrive")
+library(googledrive)
+
+project_dir <- "/Users/kirkgosik/Google Drive/My Drive/projects/"
+domice_dir <- paste0(project_dir, "domice/paper of QTL study/Revised materials of ILC-QTL paper cell Science format/")
+
+if( dir.exists("/workspace/fasi-domice/") ) {
+  
+  domice_dir <- "/workspace/fasi-domice/"
+  
+}
+
+if( dir.exists("/workspace/fasi-domice/") ) {
+  
+  domice_dir <- "/Users/kirkgosik/Documents/projects/fasi-domice/"
+  
+}
+
+data_dir <- paste0(domice_dir, "data/")
+results_dir <- paste0(data_dir, "results/")
+
+dir.create(paste0(data_dir, "references"), showWarnings = FALSE)
+dir.create(paste0(data_dir, "allchannels"), showWarnings = FALSE)
+dir.create(paste0(data_dir, "eqtl"), showWarnings = FALSE)
+dir.create(paste0(data_dir, "expression"), showWarnings = FALSE)
+dir.create(paste0(data_dir, "cytokine"), showWarnings = FALSE)
+dir.create(paste0(data_dir, "proportions"), showWarnings = FALSE)
+dir.create(paste0(data_dir, "topics"), showWarnings = FALSE)
+dir.create(paste0(data_dir, "genotype"), showWarnings = FALSE)
+
+
+
+
